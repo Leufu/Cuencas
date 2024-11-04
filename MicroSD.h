dc32d426026928bfd297e09726c07afd024760f9
nico120399 <116288190+nico120399@users.noreply.github.com>
2024-11-04 12:49:11 -0300

    Create MicroSD.h
    Parte encargada de escribir y guardar los datos del ADS1115 en la tarjeta MicroSD, haciendo uso del sistema FreeRTOS, para organizar las “tareas” en el sistema y darles diferentes prioridades

---
+++ MicroSD.h
@@ -0,0 +1,48 @@
+#ifndef MICROSD_H
+#define MICROSD_H
+
+#include <SPI.h>
+#include <SD.h>
+#include <FS.h>
+
+const int chipSelect = 10;  // Pin CS para la tarjeta SD en la ESP32 
+const char* archivo = "/test.txt";
+File dataFile;
+
+void initSD() {
+  SPI.begin(13, 12, 11, chipSelect);  // SCK=13, MISO=12, MOSI=11, CS=10
+  
+  if (!SD.begin(chipSelect)) {  
+    Serial.println("Fallo al montar la tarjeta SD.");
+    return;
+  }
+  Serial.println("Tarjeta SD montada correctamente.");
+}
+
+void writeHeader() {
+  if (!SD.exists(archivo)) {
+    dataFile = SD.open(archivo, FILE_WRITE);
+    if (dataFile) {
+      dataFile.println("Lectura de datos desde el ADS1115:");
+      dataFile.println("ADC Value | Voltage (V)");
+      dataFile.close();
+      Serial.println("Cabecera escrita correctamente.");
+    } else {
+      Serial.println("Error al abrir el archivo para escritura.");
+    }
+  }
+}
+
+void saveData(int16_t rawValue) {
+  dataFile = SD.open(archivo, FILE_APPEND);
+  if (dataFile) {
+    dataFile.print("ADS0 : ");
+    dataFile.println(rawValue); 
+    dataFile.close();
+    Serial.println("Datos guardados en la tarjeta SD.");
+  } else {
+    Serial.println("Error al abrir el archivo para escritura.");
+  }
+}
+
+#endif
